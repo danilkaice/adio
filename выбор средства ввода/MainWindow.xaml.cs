f5e6fe7ac599e4da921ff1294394ed120652d87f
danilkaice <icejami@yandex.ru>
2022-10-13 10:28:19 +0500

    audio

---
+++ выбор средства ввода/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.CoreAudioApi;
+namespace выбор_средства_ввода
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var devices = new MMDeviceEnumerator();
+            foreach (var device in devices.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active))
+            {
+                lb.Items.Add(device);
+            }
+
+        }
+      
+    }
+}
